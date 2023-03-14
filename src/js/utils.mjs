01eaf6b6c35941580d3ddbf62212b23d78f0c3cd
Von Souza <von.souza@impactzero.pt>
2023-03-14 22:54:19 +0000

    Alert message implemented when the checkout form is not filled correctly. Also, when some item has been added, an alert gives feedback to the user

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -94,3 +94,18 @@ export function removeAllAlerts() {
   const alerts = document.querySelectorAll(".alert");
   alerts.forEach((alert) => document.querySelector("main").removeChild(alert));
 }
+
+export function alertMessage(message, scroll = true, duration = 6000) {
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+  alert.innerHTML = `<p>${message}</p> <span id="myX">X</span>`;
+
+  alert.addEventListener("click", function (e) {
+    if (e.target.tagName == "SPAN") {
+      main.removeChild(this);
+    }
+  });
+  const main = document.querySelector("main");
+  main.prepend(alert);
+  if (scroll) window.scrollTo(0, 0);
+}
